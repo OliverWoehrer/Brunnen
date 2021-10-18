044097691bf7b669c8c8f137616a38b343548a19
Oliver Wöhrer <e11907563@student.tuwien.ac.at>
2021-10-18 17:56:16 +0200

    removed additional folders

--- build/core/libb64/cdecode.c.d
+++
@@ -1,7 +0,0 @@
-c:\Users\olive\Documents\Projekte\Brunnen\build\core\libb64\cdecode.c.o: \
- C:\Users\olive\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4\cores\esp32\libb64\cdecode.c \
- C:\Users\olive\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4\cores\esp32\libb64\cdecode.h \
- C:\Users\olive\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4/tools/sdk/include/newlib/stdint.h \
- C:\Users\olive\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4/tools/sdk/include/newlib/machine/_default_types.h \
- C:\Users\olive\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4/tools/sdk/include/newlib/sys/features.h \
- C:\Users\olive\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4/tools/sdk/include/newlib/sys/_intsup.h
--- build/core/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-c:\Users\olive\Documents\Projekte\Brunnen\build\core\libb64\cencode.c.o: \
- C:\Users\olive\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4\cores\esp32\libb64\cencode.c \
- C:\Users\olive\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4\cores\esp32\libb64\cencode.h
