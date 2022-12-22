8ce5d718e984fc58d803acefdbee109d66c972f5
Oliver Wöhrer <e11907563@student.tuwien.ac.at>
2022-12-22 12:34:56 +0100

    introduced heap watcher to manage heap-heavy tasks

--- src/code.cpp
+++ src/code.cpp
@@ -20,21 +20,33 @@
 //===============================================================================================
 // SCHEDULED TASKS
 //===============================================================================================
-#define TRANSMISSION_PERIODE (1000 * 60 * 60) // loop period in ms
-#define SERVICE_PERIOD (1000 * 60) // loop period in ms
-#define MEASUREMENT_PERIOD 1000 // loop period in ms
+#define TRANSMISSION_PERIODE (1000 * 60 * 60) // loop period in ms, once every hour
+#define SERVICE_PERIOD (1000 * 60) // loop period in ms, once per minute
+#define MEASUREMENT_PERIOD 1000 // loop period in ms, once per second
+#define DEFAULT_STACK_SIZE (1024 * 4) // stack size in bytes
 
 TaskHandle_t buttonHandlerHandle = NULL;
+TaskHandle_t heapWatcherHandle = NULL;
 
+/**
+ * This function implements the buttonHandlerTask with an (blocking) infinite loop and gets
+ * resumed as soon as the button is either short or long pressed. This means the task suspends
+ * itself (e.g. blocking wait) until it gets resumed by the button handler (periodicButton) after
+ * a short/long press was detected. When resumed all button flags indicating a short/long press
+ * are checked. Altough this function is technically a loop it only runs once each time the button
+ * is pressed. This means the task is reused again (instead of creating/deleting a new task each
+ * time) and also means better performance in ISR because just resuming a task is much faster then
+ * creating a new one. 
+ * @param parameter Pointer to a parameter struct (unused for now)
+ */
 void buttonHandlerTask(void* parameter) {
-    Serial.printf("[DEBUG] Created buttonHandlerTask\r\n");
+    Serial.printf("[DEBUG] Created buttonHandlerTask on Core %d\r\n",xPortGetCoreID());
     while(1) {
         vTaskSuspend(NULL); // suspend this task, resume from button ISR
         Hardware::button_indicator_t btnIndicator = Hardware::getButtonIndicator();
 
         //Handle Short Button Press:
         if (btnIndicator.shortPressed) {
-            Serial.printf("[DEBUG] button short pressed\r\n");
             Hardware::resetButtonFlags();
             DataTime::logInfoMsg("toggle user interface");
             if (UserInterface::isEnabled()) { // interface is enabled, disable again
@@ -56,7 +68,6 @@ void buttonHandlerTask(void* parameter) {
 
         //Handle Long Button Press:
         if (btnIndicator.longPressed) {
-            Serial.printf("[DEBUG] button long pressed\r\n");
             Hardware::resetButtonFlags();
             DataTime::logInfoMsg("toggle relais and operating mode");
             Hardware::manuallyToggleWaterPump();
@@ -67,16 +78,28 @@ void buttonHandlerTask(void* parameter) {
     vTaskDelete(NULL); // delete task when done, don't forget this!
 }
 
+/**
+ * This function implements the requestWeatherDataTask and requests weather data from the well
+ * known OpenWeatherAPI for the location of the coordinates defined in gw.h. The task is created
+ * in the function implementing the heapWatcherTask and notifies the heapWatcherTask that it has
+ * finished before it gets deleted again afterwards.
+ * @param parameter Pointer to a parameter struct (unused for now)
+ * @note This function allocates a lot of heap memory for web requests, see heapWatcherTask for
+ * details.
+ */
 void requestWeatherDataTask(void* parameter) {
-    Serial.printf("[DEBUG] Created requestWeatherDataTask\r\n");
-
+    // Log Task Message:
+    char debugTxt[50];
+    sprintf(debugTxt,"Created requestWeatherDataTask on Core %d",xPortGetCoreID());
+    DataTime::logDebugMsg(debugTxt);
+    
     // Build String for Request URL:
     struct tm timeinfo = DataTime::loadTimeinfo();
     char currentDate[11]; // Format: "YYYY-MM-DD"
     sprintf(currentDate, "%04d-%02d-%02d",timeinfo.tm_year+1900,timeinfo.tm_mon+1,timeinfo.tm_mday);
     
     // Send Request to OpenMeteoAPI:
-    char weatherTxt[40];
+    DataTime::logInfoMsg("Requesting weather data from OpenMeteo API.");
     if (Gateway::requestWeatherData(currentDate, currentDate)) { // failed to request data
         // Write Error Message to Log:
         const char* errorMsg = Gateway::getWeatherResponse(); // response is error on fail
@@ -93,26 +116,41 @@ void requestWeatherDataTask(void* parameter) {
         int rain = Gateway::getWeatherData("precipitation");
 
         // Append Info Message to Gateway Text:
-        sprintf(weatherTxt,"It is about to rain %d mm today. ",rain);
+        char weatherTxt[60];
+        sprintf(weatherTxt,"To my knowledge it is about to rain %d mm today. ",rain);
         Gateway::addInfoText(weatherTxt);
 
         // Check the Amount of Predicted Precipitation:
-        if (rain > 2) {
-            Serial.printf("Too much rain, pause pump operation\r\n");
+        if (rain >= Hardware::getRainThresholdLevel()) {
+            Gateway::addInfoText("That's enough rain, I will pause pump operation for today. ");
+            DataTime::logInfoMsg("Too much rain, pause pump operation.");
             Hardware::pauseScheduledPumpOperation();
         } else {
-            Serial.printf("Too little rain, resume pump operation\r\n");
+            Gateway::addInfoText("That's too little rain, I will resume pump operation for today. ");
+            DataTime::logInfoMsg("Too little rain, resume pump operation.");
             Hardware::resumeScheduledPumpOperation();
-        } 
+        }
     }
-    
 
-    // Delete Task When Done:
+    // Exit This Task:
+    xTaskNotify(heapWatcherHandle,1,eSetValueWithOverwrite); // notfiy heap watcher task by setting notification value to 1
     vTaskDelete(NULL); // delete task when done, don't forget this!
 }
 
+/**
+ * This function implements the sendMailTask and sends a mail with the appropriate files (data
+ * files and log file) to the recipient defined in gw.h. The task is created in the function
+ * implementing the heapWatcherTask and notifies the heapWatcherTask that it has finished
+ * before it gets deleted again afterwards.
+ * @param parameter Pointer to a parameter struct (unused for now)
+ * @note This function allocates a lot of heap memory for web requests, see heapWatcherTask for
+ * details
+ */
 void sendMailTask(void* parameter) {
-    Serial.printf("[DEBUG] Created sendMailTask\r\n");
+    // Log Task Message:
+    char debugTxt[50];
+    sprintf(debugTxt,"Created sendMailTask on Core %d",xPortGetCoreID());
+    DataTime::logDebugMsg(debugTxt);
 
     //Build Text to Send:
     bool isNominal = Hardware::hasNominalSensorValues();
@@ -129,16 +167,14 @@ void sendMailTask(void* parameter) {
         const char* jobName = Hardware::loadJob(i);
         int freeMemory = Gateway::addData(jobName);
         Serial.printf("EMail::attachFile(%s); with %d bytes left.\r\n",jobName,freeMemory);
+        if (freeMemory <= 0) break;
     }
 
-    // Check Free Memory (File System and Heap):
+    // Check Free Memory (File System):
     DataTime::checkLogFile(1000000); // check free space for log file
-    size_t freeHeapSize = heap_caps_get_minimum_free_size(MALLOC_CAP_DEFAULT);
-    char heapInfoTxt[40];
-    sprintf(heapInfoTxt,"Largest region currently free in heap at %d bytes.\r\n",freeHeapSize);
-    Gateway::addInfoText(heapInfoTxt);
 
     // Send Data:
+    DataTime::logInfoMsg("Sending Mail.");
     if (Gateway::sendData()) { // error occured while sending mail
         const char* errorMsg = Gateway::getErrorMsg();
         DataTime::logErrorMsg("Failed to send Email, adding file to job list.");
@@ -165,33 +201,66 @@ void sendMailTask(void* parameter) {
         return;
     }
 
-    // Delete Task When Done:
+    // Exit This Task:
+    xTaskNotify(heapWatcherHandle,1,eSetValueWithOverwrite); // notfiy heap watcher task by setting notification value to 1
     vTaskDelete(NULL); // delete task when done, don't forget this!
 }
 
-void transmissionTask(void* pvParameters) {
+/**
+ * This function implements the heapWatcherTask and watches the heap usage by starting/stoping
+ * the tasks requiring a lot of heap. It is implemented as a periodic loop with a periode 
+ * length defined by TRANSMISSION_PERIODE (currently once per hour). Every two hours the
+ * sendMailTask gets created and a mail is send. Once a day (between 00:00 - 01:00) the
+ * requestWeatherDataTask is created and new data is requested.
+ * 
+ * This task makes sure that no two heap-heavy task run at the same time. The heap management is
+ * done by deciding which heap-heavy task may run and creating a mutex for heap usage. To
+ * implement this behaviour the heapWatcherTask makes use of its nofication parameter at index 0
+ * (=default), which gets either set to 1 (heap may be used by heap-heavy tasks) or set to 0
+ * (heap a already used by a heap-heavy task). This is similar to a semaphore guarding shared
+ * memory, see FreeRTOS documentation for details on this.
+ * 
+ * Before any of the heap-heavy tasks gets created this function/tasks waits at ulTaskNotifyTake()
+ * blocking wait for the notifcation parameter to be set again, e.g. heap usage is allowed. To
+ * prevent a deadlock here it is important that the heap-heavy tasks do notify this task before
+ * they finish and get deleted, to signal that they no longer require the heap usage.
+ * @param parameter Pointer to a parameter struct (unused for now)
+ * @note Loops evey hour
+ */
+void heapWatcherTask(void* parameter) {
     // Initalize Task:
     const TickType_t xFrequency = TRANSMISSION_PERIODE / portTICK_PERIOD_MS;
-    BaseType_t xWasDelayed;
     TickType_t xLastWakeTime = xTaskGetTickCount(); // initalize tick time
-    Serial.printf("[DEBUG] Created transmissionTask{periode %u sec}\r\n", xFrequency/1000);
+    Serial.printf("[DEBUG] Created heapWatcherTask{periode %u sec} on Core %d\r\n",xFrequency/1000,xPortGetCoreID());
     
     // Periodic Loop:
     while (1) {
-        xWasDelayed = xTaskDelayUntil(&xLastWakeTime,xFrequency); // wait for the next cycle, blocking
+        xTaskDelayUntil(&xLastWakeTime,xFrequency); // wait for the next cycle, blocking
         //Check for midnight:
         struct tm timeinfo = DataTime::loadTimeinfo();
-        if (timeinfo.tm_hour == 0) { //(timeinfo.tm_hour == 0 && timeinfo.tm_min == 10 && timeinfo.tm_sec == 00)
-            // Get Connection State of WiFi:
-            bool isConnected = DataTime::isWlanConnected();
-
+        if ((timeinfo.tm_hour & 0x1) == 0) { //(timeinfo.tm_hour == 0) OR ((timeinfo.tm_hour & 0x1) == 0)
             // (Re-)Connect to WiFi:
+            bool isConnected = DataTime::isWlanConnected();
             DataTime::connectWlan();
-            DataTime::logInfoMsg("sending Email");
 
-            // Create and Start Tasks for Transmission:
-            xTaskCreate(requestWeatherDataTask,"requestWeatherDataTask",2048,NULL,1,NULL);
-            xTaskCreate(sendMailTask,"sendMailTask",2048,NULL,1,NULL);
+            // Wait to Get Notified for Free Heap:
+            ulTaskNotifyTake(pdTRUE, (120*1000)/portTICK_PERIOD_MS); // blocking wait for notification up to 120 seconds
+            
+            // Create Task for Requesting Weather:
+            size_t freeHeapSize = heap_caps_get_minimum_free_size(MALLOC_CAP_DEFAULT);
+            char heapInfoTxt[60];
+            sprintf(heapInfoTxt,"Largest region currently free in heap at %d bytes.",freeHeapSize);
+            DataTime::logDebugMsg(heapInfoTxt);
+            xTaskCreate(requestWeatherDataTask,"requestWeatherDataTask",2*DEFAULT_STACK_SIZE,NULL,0,NULL); // priority 0 (same as idle task) to prevent idle task from starvation
+            
+            // Wait to Get Notified for Free Heap:
+            ulTaskNotifyTake(pdTRUE, (120*1000)/portTICK_PERIOD_MS); // blocking wait for notification up to 120 seconds
+
+            // Create Tasks for Sending Mail:
+            freeHeapSize = heap_caps_get_minimum_free_size(MALLOC_CAP_DEFAULT);
+            sprintf(heapInfoTxt,"Largest region currently free in heap at %d bytes.",freeHeapSize);
+            DataTime::logDebugMsg(heapInfoTxt);
+            xTaskCreate(sendMailTask,"sendMailTask",2*DEFAULT_STACK_SIZE,NULL,0,NULL); // priority 0 (same as idle task) to prevent idle task from starvation
 
             // Reset Connection WiFi Connection:
             if (!isConnected) DataTime::disconnectWlan(); // disconnect wifi again if not connected before
@@ -199,32 +268,45 @@ void transmissionTask(void* pvParameters) {
     }
 }
 
-void serviceTask(void* pvParameters) {
+/**
+ * This function implements the serviceTask and periodically checks the pump intervals (is it
+ * time to switch the pump on/off). It is implemented as a periodic loop with a periode length
+ * defined by SERVICE_PERIOD (currently once per minute). This means the granularity of
+ * intervalls is minutes.
+ * @param parameter Pointer to a parameter struct (unused for now)
+ * @note Loops evey minute
+ */
+void serviceTask(void* parameter) {
     // Initalize Task:
     const TickType_t xFrequency = SERVICE_PERIOD / portTICK_PERIOD_MS;
-    BaseType_t xWasDelayed;
     TickType_t xLastWakeTime = xTaskGetTickCount(); // initalize tick time
-    Serial.printf("[DEBUG] Created serviceTask{periode %u sec}\r\n", xFrequency/1000);
+    Serial.printf("[DEBUG] Created serviceTask{periode %u sec} on Core %d\r\n",xFrequency/1000,xPortGetCoreID());
     
     // Periodic Loop:
     while (1) {
-        xWasDelayed = xTaskDelayUntil(&xLastWakeTime,xFrequency); // wait for the next cycle, blocking
+        xTaskDelayUntil(&xLastWakeTime,xFrequency); // wait for the next cycle, blocking
         // Check Scheduled Pump Intervals:
         struct tm timeinfo = DataTime::loadTimeinfo();
         Hardware::managePumpIntervals(timeinfo);
     }
 }
 
-void measurementTask(void* pvParameter) {
+/**
+ * This function implements the measurementTask and periodically measures the sensor values.
+ * It is implemented as a periodic loop with a periode length defined by MEASUREMENT_PERIOD
+ * (currently once per second). This means the granularity of sensor data is seconds.
+ * @param parameter Pointer to a parameter struct (unused for now)
+ * @note Loops evey second
+ */
+void measurementTask(void* parameter) {
     // Initalize Task:
     const TickType_t xFrequency = MEASUREMENT_PERIOD / portTICK_PERIOD_MS;
-    BaseType_t xWasDelayed;
     TickType_t xLastWakeTime = xTaskGetTickCount(); // initalize tick time
-    Serial.printf("[DEBUG] Created measurementTask{periode %u sec}\r\n", xFrequency/1000);
+    Serial.printf("[DEBUG] Created measurementTask{periode %u sec} on Core %d\r\n",xFrequency/1000,xPortGetCoreID());
     
     // Periodic Loop:
     while (1) {
-        xWasDelayed = xTaskDelayUntil(&xLastWakeTime,xFrequency); // wait for the next cycle, blocking
+        xTaskDelayUntil(&xLastWakeTime,xFrequency); // wait for the next cycle, blocking
         // Indicate Loop Entry:
         Hardware::setIndexLed(HIGH);
 
@@ -244,17 +326,20 @@ void measurementTask(void* pvParameter) {
 //===============================================================================================
 // MAIN PROGRAMM
 //===============================================================================================
+/**
+ * These functions setup() and loop() are part of the arduino loopTask which gets created
+ * automatically after start up. Because the loop function is empty/unused in this application
+ * this task delets it self after successfull setup to prevent the loop() from busy idling
+ * (looping over and doing nothing) 
+ */
 void setup() {
     delay(1000); // wait for hardware on PCB to wake up
 
     Hardware::setIndexLed(HIGH);
     Serial.begin(BAUD_RATE);
 
-    // Create and Start Scheduled Tasks:
-    xTaskCreate(measurementTask,"measurementTask",2048*4,NULL,1,NULL);
-    xTaskCreate(serviceTask,"serviceTask",2048*4,NULL,2,NULL);
-    xTaskCreate(transmissionTask,"transmissionTask",2048*4,NULL,3,NULL);
-    xTaskCreate(buttonHandlerTask, "buttonHandlerTask",2048*4,NULL,1,&buttonHandlerHandle);
+    // Create Button Handler Task:
+    xTaskCreate(buttonHandlerTask, "buttonHandlerTask",DEFAULT_STACK_SIZE,NULL,1,&buttonHandlerHandle);
     configASSERT(buttonHandlerHandle);
 
     //Initialize system hardware:
@@ -288,10 +373,16 @@ void setup() {
     UserInterface::enableInterface();
     Hardware::setUILed(HIGH);
 
-    
+    // Create and Start Scheduled Tasks:
+    xTaskCreate(measurementTask,"measurementTask",DEFAULT_STACK_SIZE,NULL,1,NULL);
+    xTaskCreate(serviceTask,"serviceTask",DEFAULT_STACK_SIZE,NULL,1,NULL);
+    xTaskCreate(heapWatcherTask,"heapWatcherTask",DEFAULT_STACK_SIZE,NULL,1,&heapWatcherHandle);
+    xTaskNotify(heapWatcherHandle,1,eSetValueWithOverwrite); // notfiy heap watcher task by setting notification value to 1
 
+    // Finish Setup:
+    DataTime::logInfoMsg("Device setup.");
     Hardware::setIndexLed(LOW);
-    delay(1000);
+    vTaskDelete(NULL); // delete this task to prevent busy idling in empty loop()
 }
 
 void loop() {}
