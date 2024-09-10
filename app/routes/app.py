20bbc9e33cf978d57015f291169ff4a83bc4c0e1
OliverWoehrer <oliver.woehrer@gmail.com>
2024-09-10 18:41:08 +0200

    implemented hierarchie for main pages templates

--- app/routes/app.py
+++ app/routes/app.py
@@ -1,7 +1,8 @@
 """
 This module implements the functions to handle main routes of app (web interface).
 """
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
@@ -11,15 +12,15 @@ def index():
 
 @app.route("/dashboard", methods=["GET"])
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("main_pages/dashboard.html")
 
 @app.route("/settings", methods=["GET"])
 def settings():
-    return render_template("settings.html")
+    return render_template("main_pages/settings.html")
 
 @app.route("/update", methods=["GET"])
 def update():
-    return render_template("update.html")
+    return render_template("main_pages/update.html")
 
 @app.route("/profile", methods=["GET"])
 def profile():
