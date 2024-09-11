0df8646036bc8e4acb4cdfaa99f6b7b1b8a1f0d3
OliverWoehrer <oliver.woehrer@gmail.com>
2024-09-11 15:32:02 +0200

    implemented data exchange for brunnen

--- app/routes/api/api.py
+++ app/routes/api/api.py
@@ -2,18 +2,26 @@
 This module implements the functions to handle routes of /api
 """
 from flask import Blueprint, render_template, redirect, url_for, abort, request
-from werkzeug.exceptions import BadRequest, Forbidden, MethodNotAllowed, UnprocessableEntity, BadGateway
-from datetime import datetime, timedelta
+from werkzeug.exceptions import BadRequest, Forbidden, NotFound, MethodNotAllowed, UnprocessableEntity, BadGateway
+from datetime import datetime, timedelta, timezone
 import json
 import pandas as pd
 from data import data_client as db
 
 api = Blueprint("api", __name__, url_prefix="/api")
 
-@api.route("/data", methods=["GET"])
-@api.route("/data/<device_id>", methods=["POST", "DELETE"])
-def data(device_id: str = None):
+@api.route("/brunnen", methods=["GET", "POST", "DELETE"])
+def brunnen():
+    # Check Access Token:
     # TODO: check if secret token is present
+    # (msg,df) = db.querySettings() # read latest settings
+    # if df is None:
+    #     raise BadGateway(("Problem while reading settings: "+msg))
+    # idx = df["devices"].last_valid_index()
+    # latest_devices = df["devices"][idx]
+    # devices_json = json.loads(latest_devices)
+    # if device_id not in devices_json["device_ids"]:
+    #     raise Forbidden(("Unknown device id: "+str(device_id)))
 
     # Parse Start Parameter:
     start_param = request.args.get("start")
@@ -38,10 +46,11 @@ def data(device_id: str = None):
         raise UnprocessableEntity("Invalid time period: Stop time has to be larger then start time.")
 
     if request.method == "GET":
-        # Read Measurement Data:
+        # TODO: change response to match other methods
+        # Read Data Data:
         total_period = stop - start
         aggregation_period = timedelta(seconds = total_period.total_seconds() // 100)
-        (msg,df) = db.queryMeasurements(start_time=start, stop_time=stop, window_size=aggregation_period)
+        (msg,df) = db.queryData(start_time=start, stop_time=stop, window_size=aggregation_period)
         if df is None:
             raise BadGateway(("Problem while reading data: "+msg))
         if df.empty:
@@ -58,58 +67,107 @@ def data(device_id: str = None):
             payload[column] = column_json["data"]
         return payload
 
-    # Check Device ID:
-    (msg,df) = db.querySettings() # read latest settings
-    if df is None:
-        raise BadGateway(("Problem while reading settings: "+msg))
-    idx = df["devices"].last_valid_index()
-    latest_devices = df["devices"][idx]
-    devices_json = json.loads(latest_devices)
-    if device_id not in devices_json["device_ids"]:
-        raise Forbidden(("Unknown device id: "+str(device_id)))
-
     if request.method == "POST":
         # Parse Request Body:
         body = request.data.decode("utf-8")
         payload = json.loads(body)
-        if "columns" not in payload:
-            raise UnprocessableEntity("Missing 'columns' field.")
-        COLUMNS = ["flow", "pressure", "level"]
-        if not all(column in COLUMNS for column in payload["columns"]):
-            raise UnprocessableEntity("Missing required column.")
-        if "data" not in payload:
-            raise UnprocessableEntity("Missing 'data' field.")
-        for row in payload["data"]:
-            if len(payload["columns"]) is not len(payload["data"][row]):
-                raise UnprocessableEntity("Number of given columns and actual data columns does not match.")
-            break
+        if "data" in payload:
+            # Check JSON Fields:
+            data = payload["data"]
+            if "columns" not in data:
+                raise UnprocessableEntity("Missing 'columns' field.")
+            if "values" not in data:
+                raise UnprocessableEntity("Missing 'values' field.")
+            for row in data["values"]:
+                if len(data["columns"]) is not len(data["values"][row]):
+                    raise UnprocessableEntity("Number of given columns and actual value columns does not match.")
+                break
 
-        # Initalize Dataframe:
-        df = pd.DataFrame.from_dict(payload["data"], orient="index", columns=payload["columns"])
-        df = df.set_index(pd.to_datetime(df.index)) # convert to datetime
+            # Initalize Dataframe:
+            df = pd.DataFrame.from_dict(data["values"], orient="index", columns=data["columns"])
+            df = df.set_index(pd.to_datetime(df.index)) # convert to datetime
 
-        # Check Parameters:
-        if start != df.index[0]:
-            raise UnprocessableEntity("Parameter 'start' does not match with the start of the given data.")
-        if stop != df.index[-1]:
-            raise UnprocessableEntity("Parameter 'stop' does not match with the stop of the given data.")
+            # Check Parameters:
+            if start != df.index[0]:
+                raise UnprocessableEntity("Parameter 'start' does not match with the start of the given data.")
+            if stop != df.index[-1]:
+                raise UnprocessableEntity("Parameter 'stop' does not match with the stop of the given data.")
 
-        # Write Measurement Data:
-        msg = db.insertMeasurements(data=df)
-        if msg:
-            raise BadGateway(("Problem while inserting data: "+str(msg)))
+            # Write Data Data:
+            msg = db.insertData(data=df)
+            if msg:
+                raise BadGateway(("Problem while inserting data: "+str(msg)))
+        
+        if "logs" in payload:
+            # Initalize Dataframe:
+            logs = payload["logs"]
+            df = pd.DataFrame.from_dict(logs, orient="index", columns=["message", "level"])
+            df = df.set_index(pd.to_datetime(df.index)) # convert to datetime
 
-        # Return Response:
-        return "ok", 200
+            # Write Logs:
+            msg = db.insertLogs(logs=df)
+            if msg:
+                raise BadGateway(("Problem while inserting logs: "+str(msg)))
+        
+        if "settings" in payload:
+            settings = payload["settings"]
+            # Initalize Dataframe:
+            timestamp = datetime.now(timezone.utc).replace(microsecond=0)
+            data = {}
+            cols = []
+            WRITEABLE_SETTINGS = ["pump"]
+            for row in settings: # check each setting if writeable
+                if row in WRITEABLE_SETTINGS:
+                    data[row] = json.dumps(settings[row])
+                    cols.append(row)
+            df = pd.DataFrame(data, index=[timestamp], columns=cols)
+
+            # Write Settings:
+            msg = db.insertSettings(settings=df)
+            if msg:
+                raise BadGateway(("Problem while inserting settings: "+str(msg)))
+                
+        # Return JSON Response:
+        (msg,df) = db.querySettings()
+        if df is None:
+            raise BadGateway(("Problem while reading settings for response: "+str(msg)))
+        payload = {}
+        payload["settings"] = {}
+        data_string = df.to_json(orient="split")
+        data_json = json.loads(data_string)
+        for setting in df.columns:
+            idx = df[setting].last_valid_index()
+            last = df[setting][idx]
+            payload["settings"][setting] = last
+
+        return payload, 200
 
     if request.method == "DELETE":
-        msg = db.deleteMeasurements(start_time=start, stop_time=stop)
+        # Drop Data:
+        msg = db.deleteData(start_time=start, stop_time=stop)
         if msg:
-            BadGateway(("Problem while deleting data: "+str(msg)))
+            raise BadGateway(("Problem while deleting data: "+str(msg)))
+        
+        # Drop Logs:
+        msg = db.deleteLogs(start_time=start, stop_time=stop)
+        if msg:
+            raise BadGateway(("Problem while deleting logs: "+str(msg)))
+
+        # Return JSON Response:
+        (msg,df) = db.querySettings()
+        if df is None:
+            raise BadGateway(("Problem while reading settings for response: "+str(msg)))
+        payload = {}
+        payload["settings"] = {}
+        data_string = df.to_json(orient="split")
+        data_json = json.loads(data_string)
+        for setting in df.columns:
+            idx = df[setting].last_valid_index()
+            last = df[setting][idx]
+            payload["settings"][setting] = last
 
         return "ok", 200
 
     else:
-        raise MethodNotAllowed(valid_methods=["GET","POST","DELETE"]) # Method Not Allowed
-
+        raise MethodNotAllowed(valid_methods=["GET","POST","DELETE"])
 
