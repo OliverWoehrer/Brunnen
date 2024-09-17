d7e0ee8b52c9c3d0438d03c7d95da153eeb031ff
OliverWoehrer <oliver.woehrer@gmail.com>
2024-09-18 00:56:38 +0200

    wrote some doc comments, rendering of error messages only for web pages (not entire app)

--- app/routes/web/web.py
+++ app/routes/web/web.py
@@ -1,21 +1,27 @@
 """
-This module implements the functions to handle routes of web web.
+This module implements the prefix and postfix request handlers for main pages. It only provides
+functions that are included before and after the actual request. It can be seen as a wrapper around
+the actual request handling code. All blueprints registered here are protected by authentication
+and tracked.
 """
-from flask import Blueprint, render_template, redirect, request, url_for, session, flash
+from flask import Blueprint, redirect, request, url_for, session, flash
 from werkzeug.exceptions import Unauthorized
 from datetime import datetime, timezone
+
+# Global Tracking Variables:
+last_visit = datetime.now(timezone.utc).replace(microsecond=0)
+visit_count = 0
+
+# Register Blueprint Hierarchy:
 from .dashboard import dashboard
 from .profile import profile
 from .settings import settings
 from .update import update
-
 web = Blueprint("web", __name__, template_folder="../../templates", static_folder="../../static")
 web.register_blueprint(dashboard)
 web.register_blueprint(profile)
 web.register_blueprint(settings)
 web.register_blueprint(update)
-last_visit = datetime.now(timezone.utc).replace(microsecond=0)
-request_count = 0
 
 @web.before_request
 def check_authentication():
@@ -25,21 +31,17 @@ def check_authentication():
         next_url = request.path
         return redirect(url_for("login", next=next_url))
 
-@web.route("/", methods=["GET"])
-def index():
-    return redirect(url_for("web.dashboard.index"))
-
 @web.after_request
 def log_request(response):
-    global request_count, last_visit
-    request_count = request_count + 1
+    global visit_count, last_visit
+    visit_count = visit_count + 1
     last_visit = datetime.now(timezone.utc).replace(microsecond=0)
     return response
 
 @web.context_processor
-def inject_request_counter():
-    global request_count
-    return dict(debug=request_count)
+def inject_visit_counter():
+    global visit_count
+    return dict(debug=visit_count)
 
 def get_last_visit() -> datetime:
     global last_visit
