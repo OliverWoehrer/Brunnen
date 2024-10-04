8a773cbc6e2f601fa0546448a9bb6be75e0288ff
OliverWoehrer <oliver.woehrer@gmail.com>
2024-10-04 14:44:24 +0200

    refactoring of request structure, use fetch instead if jQuery

--- app/static/dashboard.js
+++ app/static/dashboard.js
@@ -11,30 +11,73 @@
  * 
  */
 
+// Color Codes:
 const LEVEL_COLOR = "#A5A5A5";
 const PRESSURE_COLOR = "#ED7D31";
 const FLOW_COLOR = "#4472C4";
 
+// Scaling Factors:
+const FLOW_SCALING = 0.002577; // 388 pulses per litre
+const PRESSURE_SCALING = 0.002513; // 398 increments per bar
+const LEVEL_SCALING = 0.00157356; // 635 increments per meter
+
+
 
 
 //===============================================
-// Fetching Window Times
+// Updating Charts
 //===============================================
 
-async function requestLatestTimestamp() {
-    let promise = $.ajax({ 
-        url: "/api/web/sync", 
-        type: "get",
-        accepts: "application/json",
-    });
-    return promise;
+/**
+ * Fetches log messages between start and stop and adds them to the table element with the given
+ * ID
+ * @param {Date} start erliest date-time of logs to fetch
+ * @param {Date} stop latest date-time of logs to fetch
+ * @param {String} tableID id of the table element
+ */
+async function updateLogs(start, stop, tableID) {
+    const {columns, data} = await fetchLogs(start, stop);
+    const logsTable = document.getElementById(tableID);
+    fillTable(logsTable, columns, data);
 }
 
-async function getLatestTimestamp() {
-    const res = await requestLatestTimestamp();
-    const lastSync = res["last_sync"];
-    return new Date(lastSync);
+/**
+ * Fetches data between start and stop and adds it to the charts given
+ * @param {Date} start erliest date-time of logs to fetch
+ * @param {Date} stop latest date-time of logs to fetch
+ * @param {JSCharting} lineChart object holding the line chart
+ * @param {JSCharting} gaugesChart object holding the gauges chart
+ */
+async function updateData(start, stop, lineChart, gaugesChart) {
+    const datasets = await fetchData(start, stop);
+    if(typeof gaugesChart !== 'undefined') {
+        let flow = 0;
+        let pressure = 0;
+        let level = 0;
+        for(const dataset of datasets) {
+            // Points: [[Date(),value], [Date(),value], ...]
+            const lastPoint = dataset.points[dataset.points.length - 1];
+            last = parseFloat(lastPoint[1].toFixed(1));
+            if(dataset.name == "Flow") flow = last;
+            if(dataset.name == "Pressure") pressure = last;
+            if(dataset.name == "Level") level = last;
+        }
+        updateGauges(gaugesChart, flow, pressure, level);
+    }
+    if(typeof lineChart !== 'undefined') {
+        // Points: [[Date(),value], [Date(),value], ...]
+        const dataset = datasets[0];
+        const firstPoint = dataset.points[0];
+        const lastPoint = dataset.points[dataset.points.length - 1];
+
+        const options = { year:"2-digit", month:"2-digit", day:"2-digit" };
+        const firstLabel = firstPoint[0].toLocaleDateString("de-AT", options);
+        const lastLabel = lastPoint[0].toLocaleDateString("de-AT", options);
+        updateLines(lineChart, firstLabel+" - "+lastLabel, datasets);
+    }
+    
 }
+
 
 
 
@@ -42,62 +85,108 @@ async function getLatestTimestamp() {
 // Data Fetching
 //===============================================
 
-function requestLogs(startTime, stopTime) {
-    startString = startTime.toISOString().replace("Z","");
-    stopString = stopTime.toISOString().replace("Z","");
-    $.ajax({ 
-        url: "/api/web/logs",
-        type: "get",
-        accepts: "application/json",
-        data: { start: startString, stop: stopString },
-        success: loadLogs,
-        error: alertErrorResponse
-    });
+/**
+ * Make an asynchronious request to the backend for the timestamp of the latetest data.
+ * @returns an async promise for the request
+ */
+async function fetchLatestTimestamp() {
+    let response;
+    try {
+        response = await fetch("/api/web/sync", {
+            method: "GET",
+            headers: { Accept: "application/json" }
+        });
+        if(!response.ok) {
+            const res = await response.text();
+            const msg = parseErrorPage(res);
+            throw Error("Server Response: "+msg);
+        }
+    } catch(error) {
+        throw Error("Problem while fetching timestamp: "+error);
+    }
+
+    try {
+        const res = await response.json();
+        const lastSync = res["last_sync"];
+        return new Date(lastSync);
+    } catch(error) {
+        throw Error("Failed to parse timestamp: "+error);
+    }
 }
 
-function loadLogs(res) {
-    // Parse Reponse:
-    columns = res["columns"];
-    data = res["data"];
+/**
+ * Make an asynchronious request to the backend to request log messages between start and stop.
+ * @param {Date} start earliest date-time of logs to fetch
+ * @param {Date} stop latest date-time of logs to fetch
+ * @returns object of column names and actual logs: { columns: [...], data: [...] }
+ */
+async function fetchLogs(start, stop) {
+    // Request Logs:
+    let response;
+    try {
+        startString = start.toISOString().replace("Z","");
+        stopString = stop.toISOString().replace("Z","");
+        const params = new URLSearchParams({ start: startString, stop: stopString });
+        response = await fetch("/api/web/logs"+"?"+params, {
+            method: "GET",
+            headers: { Accept: "application/json" }
+        });
+    } catch(error) {
+        throw Error("Problem while fetching logs: "+error);
+    }
 
-    // Get DOM Element for Table:
-    let logsTable = document.getElementById("logs_table");
+    // Parse Response:
+    if(!response.ok) {
+        const res = await response.text();
+        const msg = parseErrorPage(res);
+        throw Error(msg);
+    }
+    const logs = await response.json();
+    if(isEmpty(logs)) {
+        throw Error("No logs returned.");
+    }
+    return { columns: logs["columns"], data: logs["data"] };
+}
 
-    // Hide Waiting Animation:
-    hideElement("logs_table_waiting");
+/**
+ * Make an asynchronious request to the backend to request data between start and stop.
+ * @param {Date} start earliest date-time of data to fetch 
+ * @param {Date} stop latest date-time of data to fetch
+ * @returns array of datasets: [{name: "Flow", color: #F0C5B5, points: [...]}, ...]
+ */
+async function fetchData(start, stop) {
+    let response;
+    try {
+        startString = start.toISOString().replace("Z","");
+        stopString = stop.toISOString().replace("Z","");
+        const params = new URLSearchParams({ start: startString, stop: stopString });
+        response = await fetch("/api/web/data"+"?"+params, {
+            method: "GET",
+            headers: { Accept: "application/json" }
+        });
+    } catch(error) {
+        throw Error("Problem while fetching logs: "+error);
+    }
     
-    // Insert User as Table Rows:
-    plotLogs(logsTable, columns, data);
-}
-
-function requestData(startTime, stopTime) {
-    startString = startTime.toISOString().replace("Z","");
-    stopString = stopTime.toISOString().replace("Z","");
-    $.ajax({ 
-        url: "/api/web/data", 
-        type: "get",
-        accepts: "application/json",
-        data: { start: startString, stop: stopString },
-        success: loadData,
-        error: alertErrorResponse
-    });
-}
-
-function loadData(res) {
-    //Declare Global Variables:
-    const FLOW_SCALING = 0.002577; // 388 pulses per litre
-    const PRESSURE_SCALING = 0.002513; // 398 increments per bar
-    const LEVEL_SCALING = 0.00157356; // 635 increments per meter
 
     // Parse Reponse:
-    columnNames = Object.keys(res);
+    if(!response.ok) {
+        const res = await response.text();
+        const msg = parseErrorPage(res);
+        throw Error(msg);
+    }
+    const data = await response.json();
+    if(isEmpty(data)) {
+        throw Error("No data returned.");
+    }
+    columnNames = Object.keys(data);
 
     // Parse Labels from Response:
     let labels;
     let titleText = ["No Data Received"];
-    if("Time" in res) {
+    if("Time" in data) {
         // Parse Labels from Response:
-        labels = Object.values(res["Time"]);
+        labels = Object.values(data["Time"]);
         
         // Build Canvas Title:
         const firstLabel = labels[0];
@@ -108,10 +197,11 @@ function loadData(res) {
         labels = labels.map(function(label) { return new Date(label); });
     }
 
+    // Build Datasets:
     let datasets = new Array();
     let latest = {}
-    if("Flow" in res) {
-        let values = Object.values(res["Flow"]);
+    if("Flow" in data) {
+        let values = Object.values(data["Flow"]);
         values = values.map(function(value) { return value*FLOW_SCALING; }); // scale all values
         const latestValue = values[values.length - 1];
         latest["flow"] = parseFloat(latestValue.toFixed(1));
@@ -123,8 +213,8 @@ function loadData(res) {
         }
         datasets.push(set);
     }
-    if("Pressure" in res) {
-        values = Object.values(res["Pressure"]);
+    if("Pressure" in data) {
+        values = Object.values(data["Pressure"]);
         values = values.map(function(value) { return value*PRESSURE_SCALING; });
         const latestValue = values[values.length - 1];
         latest["pressure"] = parseFloat(latestValue.toFixed(1));
@@ -136,8 +226,8 @@ function loadData(res) {
         }
         datasets.push(set);
     }
-    if("Level" in res) {
-        values = Object.values(res["Level"]);
+    if("Level" in data) {
+        values = Object.values(data["Level"]);
         values = values.map(function(value) { return value*LEVEL_SCALING; });
         const latestValue = values[values.length - 1];
         latest["level"] = parseFloat(latestValue.toFixed(1));
@@ -150,25 +240,7 @@ function loadData(res) {
         datasets.push(set);
     }
 
-    // Plot Data:
-    if(typeof gaugesChart !== 'undefined') {
-        updateGauges(latest["flow"], latest["pressure"], latest["level"]);
-    }
-    if(typeof lineChart !== 'undefined') {
-        updateLines(titleText, datasets);
-    }
-}
-
-/**
- * This as a common error-callback function for the request functions. It simply parses the
- * response and displays the error message.
- * @param {Object} res response object
- */
-function alertErrorResponse(res) {
-    const doc = new DOMParser().parseFromString(res.responseText, "text/html");
-    let title = doc.getElementsByTagName("title");
-    let nodes = doc.getElementsByTagName("p");
-    window.alert("Server Response: ["+title[0].innerText+"] "+nodes[0].innerText);
+    return datasets;
 }
 
 
@@ -178,10 +250,13 @@ function alertErrorResponse(res) {
 // Data Visualization (Action Functions)
 //===============================================
 
-function plotLogs(table, columns, data)  {
-    fillTable(table, columns, data); // action call
-}
-
+/**
+ * Generates a JSCharting object in the given container. Should be called once on page load and can
+ * be filled with the matching update function.
+ * @see updateGauges
+ * @param {HTMLElement} gaugesChartDiv chart container to plot into
+ * @returns JSCharting object of the newly generate chart
+ */
 function plotGauges(gaugesChartDiv) {
     // Colors Palette:
     const bodyStyles = window.getComputedStyle(document.body);
@@ -225,13 +300,19 @@ function plotGauges(gaugesChartDiv) {
     }
 
     // Build Chart:
-    gaugesChart = JSC.chart(gaugesChartDiv, config);
+    return JSC.chart(gaugesChartDiv, config);
 }
 
-function updateGauges(flow, pressure, level) {
-    // Hide Waiting Animation:
-    hideElement("gaugesChart_waiting");
-
+/**
+ * Updates the given chart object with the given values. The chart object is returned by the matching
+ * plot function.
+ * @see plotGauges
+ * @param {JSCharting} gaugesChart object holds the chart
+ * @param {Float} flow value to plot on the flow gauge
+ * @param {Float} pressure value to plot on the pressure gauge
+ * @param {Float} levelvalue to plot on the level gauge
+ */
+function updateGauges(gaugesChart, flow, pressure, level) {
     const flowSeries = gaugesChart.series("Flow");
     const pressureSeries = gaugesChart.series("Pressure");
     const levelSeries = gaugesChart.series("Level");
@@ -240,6 +321,13 @@ function updateGauges(flow, pressure, level) {
     levelSeries.points(0).options({ y: level });
 }
 
+/**
+ * Generates a JSCharting object for lines in the given container. Should be called once on page
+ * load and can be filled with the matching update function.
+ * @see updateLines
+ * @param {HTMLElement} lineChartDiv chart container to plot into
+ * @returns JSCharting object of the newly generate chart
+ */
 function plotLines(lineChartDiv) {
     // Colors Palette:
     const bodyStyles = window.getComputedStyle(document.body);
@@ -303,29 +391,60 @@ function plotLines(lineChartDiv) {
 
     // Build Chart:
     if(lineChart) { lineChart = {} }
-    lineChart = JSC.chart(lineChartDiv, config);
-
-    // Hide Waiting Animation:
-    hideElement("lineChart_waiting");
+    return JSC.chart(lineChartDiv, config);
 }
 
-function updateLines(titleText, datasets) {
-    // Hide Waiting Animation:
-    hideElement("lineChart_waiting");
-
+/**
+ * Updates the given chart object with the given values. The chart object is returned by the matching
+ * plot function.
+ * @see plotLines
+ * @param {JSCharting} lineChart object holds chart
+ * @param {String} titleText title of the chart
+ * @param {Array} datasets array of datasets: [{name: "Flow", color: #F0C5B5, points: [...]}, ...]
+ */
+function updateLines(lineChart, titleText, datasets) {
     // Set Title:
     lineChart.options({title: {label: {text: titleText}}});
 
     for(const dataset of datasets) {
         // Concatenate Existing and New Points:
-        let series = lineChart.series(dataset.name);
-        const newPoints = dataset.points;
+        const series = lineChart.series(dataset.name);
         const exisitingPoints = series.points().items;
+        const newPoints = dataset.points;
         dataset.points = exisitingPoints.concat(newPoints);
 
         // Replace Entire Series:
-        series.remove();
+        lineChart.series(dataset.name).remove();
         lineChart.series.add(dataset);
     }
 }
 
+
+
+//===============================================
+// Helper Functions:
+//===============================================
+
+/**
+ * Parses the common error page and extracts the error message.
+ * @param {String} page html string of error page
+ * @return error message as string
+ */
+function parseErrorPage(page) {
+    const doc = new DOMParser().parseFromString(page, "text/html");
+    let title = doc.getElementsByTagName("title");
+    let nodes = doc.getElementsByTagName("p");
+    return "["+title[0].innerText+"] "+nodes[0].innerText;
+}
+
+/**
+ * Checks if the given object is empty ({})
+ * @param {Object} obj object to check
+ * @returns true if the given object is empty, false otherwise
+ */
+function isEmpty(obj) {
+    for (const prop in obj) {
+        if (Object.hasOwn(obj, prop)) return false;
+    }
+    return true;
+}
